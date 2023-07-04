4389aa1b7ea90a979ada948b2ac3df282e78077b
b-lemy <lemabrian1234@gmail.com>
2023-07-04 13:09:52 -0700

    advanced tasks map set weakmaps

---
+++ 100-weak.js
@@ -0,0 +1,13 @@
+
+#!/usr/bin/python3
+rt const weakMap = new WeakMap();
+let count = 1;
+
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
